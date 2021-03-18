c8d49ae7d6e7ae5b46859ceace1ae31793d958c0
Maks-Chornyi <maksym.chornyi@oa.edu.ua>
2021-03-18 13:43:02 +0200

    added simple project with oauth2 login

---
+++ src/main/java/com/makschornyi/googledemo/Controller.java
@@ -0,0 +1,15 @@
+package com.makschornyi.googledemo;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String hello(@AuthenticationPrincipal OidcUser oAuth2User) {
+        return "hello";
+    }
+}
---
+++ src/main/java/com/makschornyi/googledemo/GoogledemoApplication.java
@@ -0,0 +1,13 @@
+package com.makschornyi.googledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogledemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogledemoApplication.class, args);
+	}
+
+}
